8a00e9da680bae40b947f9dfc51156d894c19810
ghecko <ghecko78@gmail.com>
2020-02-11 23:26:25 +0100

    Errors correction

--- update_framework.py
+++ update_framework.py
@@ -22,7 +22,7 @@ def main(pid, logfile):
     :return: Nothing
     """
     with open(logfile, "w") as f:
-        f.write("Start waiting for the completion of the calling process (owfupdate).")
+        f.write("Start waiting for the completion of the calling process (owfupdate).\n")
         timeout = time.time() + 60
         while time.time() < timeout:
             if not psutil.pid_exists(pid):
@@ -30,16 +30,16 @@ def main(pid, logfile):
                                          stderr=subprocess.PIPE)
                 stdout, stderr = pipes.communicate()
                 if pipes.returncode != 0:
-                    logfile.write("{}[✘]{}Error while updating the octowire-framework package: {}"
-                                  .format(Colors.FAIL, Colors.ENDC, stderr.strip()))
+                    f.write("{}[X]{}Error while updating the octowire-framework package: {}\n"
+                            .format(Colors.FAIL, Colors.ENDC, stderr.strip()))
                 else:
-                    logfile.write("{}[✔]{}The octowire-framework was successfully updated.".format(Colors.OKGREEN,
-                                                                                                   Colors.ENDC))
+                    f.write("{}[V]{}The octowire-framework was successfully updated.\n".format(Colors.OKGREEN,
+                                                                                             Colors.ENDC))
                 break
         else:
-            logfile.write("{}[✘]{}Timeout reached while waiting for the completion of the calling process (owfupdate)."
-                          " Please run the 'owfupdate' command again to try to update the octowire-framework package."
-                          .format(Colors.FAIL, Colors.ENDC))
+            f.write("{}[X]{}Timeout reached while waiting for the completion of the calling process (owfupdate)."
+                    " Please run the 'owfupdate' command again to try to update the octowire-framework package.\n"
+                    .format(Colors.FAIL, Colors.ENDC))
 
 
 if __name__ == '__main__':
@@ -47,7 +47,6 @@ if __name__ == '__main__':
                                                  "called by the 'owfupdate' command. Run it at your own risk.{}".format(
                                                   Colors.BOLD, Colors.ENDC))
     parser.add_argument("-p", "--pid", help="The pid of the parent process.", type=int, required=True)
-    parser.add_argument("-f", "--logfile", help="The path of the file to log the update process status",
-                        type=int, required=True)
+    parser.add_argument("-f", "--logfile", help="The path of the file to log the update process status", required=True)
     args = parser.parse_args()
     main(args.pid, args.logfile)
