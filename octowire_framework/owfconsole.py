7a794541eb0364f7bceba34b575457db4ee9c7ca
ghecko <ghecko78@gmail.com>
2020-02-20 23:49:15 +0100

    Manage logo on windows console

--- octowire_framework/owfconsole.py
+++ octowire_framework/owfconsole.py
@@ -8,9 +8,12 @@
 
 
 import argparse
+import os
+import platform
 import shutil
 
 from octowire_framework.core.Framework import Framework
+from octowire.utils.Colors import Colors
 from octowire.utils.Logger import Logger
 
 
@@ -19,7 +22,7 @@ def welcome(terminal_width):
     Print the framework header.
     :return: Nothing
     """
-    logo = """\x1b[38;5;92m
+    logos = ["""\x1b[38;5;92m
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣴⣶⣿⣿⣿⣿⣶⣦⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠀
@@ -44,8 +47,68 @@ def welcome(terminal_width):
 ⢠⣴⡶⣶⡄⠀⢠⣴⡶⡶⠀⣿⣷⣶⠀⢠⣴⢶⣦⡄⢰⣶⠀⢰⣦⠀⣴⡖⢰⣶⠀⢠⣶⠶⡆⢀⣴⠶⣶⡄
 ⣿⡇⠀⢸⣿⠀⣿⡇⠀⠀⠀⣿⡇⠀⠀⣿⡇⠀⢸⣿⠀⢿⡇⣿⢿⣤⣿⠁⢸⣿⠀⢸⣿⠀⠀⣾⡿⠶⠾⠿
 ⠙⢿⣶⡾⠏⠀⠹⢷⣶⣶⠀⠻⣷⣶⡄⠹⢷⣶⡾⠏⠀⠘⣿⠇⠘⣿⠇⠀⢸⣿⠀⢸⣿⠀⠀⠘⢿⣶⣶⡆
-        \x1b[0m"""
-    print('\n'.join(line.center(terminal_width) for line in logo.splitlines()))
+            \x1b[0m""",
+             """{}
+                      ~yNNNNHNNNNy~.                         
+                   oNHHHHHHHHHHHHHHHNw                       
+                 (NHHHHHHHHHHHHHHHHHHHNp                     
+                (HHHHHHHHHHHHHHHHHHHHHHHb                    
+                NHHHHHHHHHHHHHHHHHHHHHHHH                    
+                #HHHHHHHHHHHHHHHHHHHHHHHH                    
+                `HHHHHHHHHHHHHHHHHHHHHHHL                    
+                 (HHHHHHHHHHHHHHHHHHHHHN                     
+      ~yy,       (HCVHHHHHHHHHHHHHHHN"HN       ,;y~          
+     NH `HN      (NN,  "NHHHHHHHN"   NHN      @H" NH         
+  (NN7HNNHN        7Hy. .#HHHHHN, .~NN        "HNNNN7Nw      
+ &HH             .,~pHHHHHHHHHHHHHHHN~~.             @HN     
+ (HHN~ ,,,~ypNNHHMNHHHHHHHHHHHHHHHHHHHNNNHNNNy~,,, ,NHHN     
+  "NHHHNNHHHHHN.#NHHHHHHHHHHHHHHHHHHHHHHN,7NHHHHNNHHHH"      
+      \"\"\"\"\"   NHHHHHHHHHHHHHHHHHHHHHHHHHHHN,  `"\"\"\"        
+  NHNNN      NHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH,     #HNNN,     
+ (Hb./HL    NHNHHHNHHHHHHM"   `7NHHHHHNHHHNNH    (HN.(HN     
+  (HM=     NH NHHH HHHHH  pNNNN  NHHHH)NHHH,HN,    "7HN      
+  NH,    ,NHN(HHHH(HHHHL (Hb (HN (HHHH)&HHHN(HH\\     HH      
+  "HHwy0NHHH &HHHH HHHHb  "MNN"  (HHHH NHHHH HHHNNyyNHM      
+    "*NNNM"  (HHHH,(HHHN         NHHHN NHHHH  "7NNN="        
+             (HHHHN VNHHN       #HHHN NHHHHN                 
+     .~;~    (HHHHH  7HHHN,    NHHHN  NHHHHN    ~;~,         
+    (HC`7H,  (HHHHN   (HHHH   NHHHN   (HHHHN   NN "HN        
+    (HNyNN   NHHHH     "HHHN (HHHL     NHHHH   NNwNHL        
+    Nb      (HHHN       (HHH &HHN       NHHHb      (N        
+    "HNw;;^qHHHN         HH' `HH         "NHHN`;;yNHN        
+      "VNNHNM"        .NNH'   `NMN,        `7NHNNM"          
+              ;0Ny .;NHN"       "NHNy, ;0Ny                  
+             NH  NH="               "*HH  AH                 
+             "NNNH"                   "NNNH"                 
+
+
+                ,~                      #N                   
+  .w#m,   .o#m, #Nmo,  ~mmo  ;o  :o  oo :m  ,ommc  omm,      
+ #N   N# (NE    #N    #R  YN  NE #N, NE (N  #N   (NE..N#     
+ AN, .NR (N,    #N    N#  #N  'N#E`N#R  (N  #N   `NE         
+  `4MM"   `7MM=  7MM*  "MM=    "=  "=   *=  *=     7MMN'     
+             {}""".format(Colors.MAGENTA, Colors.ENDC)
+             ]
+    environ = os.environ
+    # Check if Linux WSL or Windows but not in a Windows Terminal session
+    if (("WT_SESSION" not in environ and (("Linux" in platform.uname()[0] and "Microsoft" in platform.uname()[2]) or
+         "Windows" in platform.system()))):
+        print('\r'.join(line.center(terminal_width) for line in logos[1].splitlines()))
+    else:
+        # Check if the distribution is a Linux system and it has the TERM environment variable (WSL or Not)
+        if "Linux" in platform.system() and "TERM" in environ:
+            # Linux without x-term256color support
+            if environ["TERM"] != "xterm-256color":
+                print('\n'.join(line.center(terminal_width) for line in logos[1].splitlines()))
+            # Linux with x-term256color support
+            else:
+                print('\n'.join(line.center(terminal_width) for line in logos[0].splitlines()))
+        # Windows in a Windows Terminal session
+        elif "Windows" in platform.system():
+            print('\n'.join(line.center(terminal_width) for line in logos[0].splitlines()))
+        # Other
+        else:
+            print('\n'.join(line.center(terminal_width) for line in logos[1].splitlines()))
 
 
 def main():
