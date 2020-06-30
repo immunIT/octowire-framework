32d83e88824da636d8140298d80f7f20e841a0c8
ghecko <ghecko78@gmail.com>
2020-06-30 11:36:09 +0200

    Add git_init script (bitbucket mirroring)

---
+++ git_init.sh
@@ -0,0 +1,3 @@
+git remote set-url origin --push --add https://github.com/immunIT/octowire-framework
+git remote set-url origin --push --add git@bitbucket.org:octowire/octowire-framework.git
+git remote -v
